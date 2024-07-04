8ea78875f6bfb972c78956d9adee90453551858f
Kerly Jaya <kerlypaulina32@gmail.com>
2024-07-04 07:38:40 -0500

    Primer commit realizado

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const app = express()
+ const router = require ('./router/router')
+ const port=3000
+
+
+ app.set('view engine', 'ejs')
+ app.use(router)
+
+ app.listen(port,()=>{
+    //console.log('Servidor coriendo en http://localhost:',port)
+    console.log(`Servidor ejecutandose http://localhost:${port}`)
+ })
